07e7c6c2872b761aca47937958fb9a63b526e1d2
Marli Renee <marlirenee@gmail.com>
2020-09-12 11:41:37 -0700

    troubleshooting heroku deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://marliree@localhost/awareapi',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://marliree@localhost/awareapi',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://marliree@localhost/awareapi-test',
 }
