388977bc90e742e8485e5aa682f1a48a4d8e501e
Marli Renee <marlirenee@gmail.com>
2020-10-02 12:31:14 -0700

    extra forward slash removed from config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://marliree@localhost/awareapi',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://marliree@localhost/awareapi-test',
-    CLIENT_ORIGIN: 'https://aware-app.vercel.app/'
+    CLIENT_ORIGIN: 'https://aware-app.vercel.app'
 }
 
-// || ''http://localhost:3000''
+// || ''http://localhost:3000'''https://aware-app.vercel.app/'
